8307b8601c91796e96738e016354e1d9ece8c2dd
ARahim-S <arahim.s@yandex.com>
2023-05-26 11:11:46 +0300

    updated

---
+++ src/main/java/day04/advancedpractice01/Q03_Hypotenuse.java
@@ -0,0 +1,18 @@
+package day04.advancedpractice01;
+
+public class Q03_Hypotenuse {
+
+        /*
+       Write a code calculate the Hypotenuse
+       Formul: Square root(a*a + b*b)
+        */
+
+    public static void main(String[] args) {
+        double a = 8;
+        double b = 5;
+
+        double hypotenuse = Math.sqrt(Math.pow(a,2)+Math.pow(b,2));
+        System.out.println("hypotenuse = " + hypotenuse);
+
+    }
+}
---
+++ src/main/java/day04/advancedpractice01/Q04_Hypotenuse_Method.java
@@ -0,0 +1,27 @@
+package day04.advancedpractice01;
+
+import java.util.Scanner;
+
+public class Q04_Hypotenuse_Method {
+
+            /*
+            Create method
+            Write a code calculate the Hypotenuse
+            Formul: Square root(a*a + b*b)
+            */
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter one edge of the triangle");
+        double edgeTriangle1 = input.nextDouble();
+        System.out.println("Enter second edge of the triangle");
+        double edgeTriangle2 = input.nextDouble();
+
+        double result = calcHypotenuse(edgeTriangle1,edgeTriangle2);
+        System.out.println("result = " + result);
+    }
+
+    public static double calcHypotenuse(double a, double b) {
+        return Math.sqrt(Math.pow(a,2) + Math.pow(b,2));
+    }
+}
