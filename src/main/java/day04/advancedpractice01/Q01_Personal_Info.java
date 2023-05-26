2c7dd4442e8434912731a365406f6a9831d3921e
ARahim-S <arahim.s@yandex.com>
2023-05-26 10:07:02 +0300

    updated

--- src/main/java/day04/advancedpractice01/Q01_Personal_Info.java
+++ src/main/java/day04/advancedpractice01/Q01_Personal_Info.java
@@ -9,9 +9,11 @@ public class Q01_Personal_Info {
         double weight = 89.02457;
 
         System.out.println("Firstname : " + firstName + "\nLastName : "
-                + lastName + "\nAge : " + age
-                + "\nHeight :" + height
-                + "\nWeight : " + weight);
+                + lastName + "\n\tAge : " + age
+                + "\n\tHeight :" + height
+                + "\n\tWeight : " + weight);
+
+        //\t ==> use for tab.
 
 
     }
---
+++ src/main/java/day04/advancedpractice01/Q02_Average.java
@@ -0,0 +1,30 @@
+package day04.advancedpractice01;
+
+import java.text.DecimalFormat;
+
+public class Q02_Average {
+            /*
+        double number1 = 23.4;
+        double number2 = 24.0;
+        double number3 = 12;
+        double number4 = 450.3;
+        double number5 = 23000;
+        Calculate average of the these numbers
+     */
+
+    public static void main(String[] args) {
+        double number1 = 23.4;
+        double number2 = 24.0;
+        double number3 = 12;
+        double number4 = 450.3;
+        double number5 = 23000;
+
+        double sum = number1 + number2 + number3 + number4 + number5;
+        double avg = sum / 5;
+        String pattern = "##.##";
+        DecimalFormat df = new DecimalFormat(pattern);
+        String format = df.format(avg);
+        System.out.println("format = " + format);
+        //DecimalFormat class= you can format your double variables. Firstly you need a pattern for give format to the variable.
+    }
+}
