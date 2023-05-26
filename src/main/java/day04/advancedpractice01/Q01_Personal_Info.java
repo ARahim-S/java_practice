9d7ab2e801305510bad26cc3de988267dce0629a
ARahim-S <arahim.s@yandex.com>
2023-05-26 09:42:42 +0300

    updated

---
+++ src/main/java/day04/advancedpractice01/Q01_Personal_Info.java
@@ -0,0 +1,18 @@
+package day04.advancedpractice01;
+
+public class Q01_Personal_Info {
+    public static void main(String[] args) {
+        String firstName = "John";
+        String lastName = "Doe";
+        int age = 39;
+        float height = 1.85f;
+        double weight = 89.02457;
+
+        System.out.println("Firstname : " + firstName + "\nLastName : "
+                + lastName + "\nAge : " + age
+                + "\nHeight :" + height
+                + "\nWeight : " + weight);
+
+
+    }
+}
