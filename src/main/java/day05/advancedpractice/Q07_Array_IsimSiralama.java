094055008ac3492197e023d4d6f4632207a124b9
ARahim-S <arahim.s@yandex.com>
2023-06-02 12:09:56 +0300

    updated

---
+++ src/main/java/day05/advancedpractice/Q07_Array_IsimSiralama.java
@@ -0,0 +1,19 @@
+package day05.advancedpractice;
+
+import java.util.Arrays;
+
+
+public class Q07_Array_IsimSiralama {
+    /*
+        Bir sınıfta bulunan öğrencilerin isimlerini içeren bir array verildiğinde,
+        bu öğrencilerin isimlerini sıralayan bir program yazın.
+        {"Ali", "Zeynep", "Mehmet", "Ayşe"}
+    */
+
+
+    public static void main(String[] args) {
+        String[] arr = {"Ali", "Zeynep", "Mehmet", "Ayşe"};
+        Arrays.sort(arr);
+        System.out.println("Sıralanmış Öğrenci Listesi: " + Arrays.toString(arr));
+    }
+}
---
+++ src/main/java/day05/advancedpractice/Q09_Array_OturmaPlani.java
@@ -0,0 +1,25 @@
+package day05.advancedpractice;
+
+import java.util.Arrays;
+
+
+public class Q09_Array_OturmaPlani {
+
+        /*
+            Bir parti organizasyonunda, katılımcıların oturma planını bir array olarak tutuyorsunuz.
+            Oturma planını yazdırdığınızda boş sandalyeler null olarak görünüyor.
+            Bunun yerine "Rezerve Değil" yazmasını istiyorsunuz.
+                String[] oturmaPlani = new String[10];
+                System.out.println(Arrays.toString(oturmaPlani));
+        */
+
+
+    public static void main(String[] args) {
+        String[] oturmaPlani = new String[10];
+        System.out.println(Arrays.toString(oturmaPlani));
+
+        String yeniDeger = "Rezerve Değil";
+        Arrays.fill(oturmaPlani, yeniDeger);
+        System.out.println(Arrays.toString(oturmaPlani));
+    }
+}
