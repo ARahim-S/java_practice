222f9a6fe6ce48f3755682ecf24c2be1de9dde39
ARahim-S <arahim.s@yandex.com>
2023-06-02 11:43:59 +0300

    updated

---
+++ src/main/java/day05/advancedpractice/Q05_Array_IsaretDegistir.java
@@ -0,0 +1,25 @@
+package day05.advancedpractice;
+
+import java.util.Arrays;
+
+public class Q05_Array_IsaretDegistir {
+
+        /*
+            Array içerisindeki elementlerin işaretlerini (+-) değiştiren bir kod yazınız.
+            input : 1,2,-3,4,-5,-6
+            output :-1,-2,3,-4,5,6
+        */
+
+    public static void main(String[] args) {
+        int[] sayilar = {1, 2, -3, 4, -5, -6};
+
+        for (int i = 0; i < sayilar.length; i++) {
+            sayilar[i] *= -1;
+        }
+
+        System.out.println(Arrays.toString(sayilar));
+
+    }
+}
+
+        
